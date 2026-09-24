d488612996b3aba40389a1393fa5e0dea5e84952
brenofernandes3 <brenofernanddes18@gmail.com>
2026-09-24 00:13:08 -0300

    Implement INSS calculation function
    Added a function to calculate INSS based on salary brackets.

--- Aliquota_INSS.c
+++ Aliquota_INSS.c
@@ -1,4 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+int main(int argc, char *argv[]) {
+	
+	float calcularINSS(float salario) {
+    float inss;
+
+    if (salario <= 1518.00) {
+        inss = salario * 0.075;
+    }
+    else if (salario <= 2793.88) {
+        inss = salario * 0.09;
+    }
+    else if (salario <= 4190.83) {
+        inss = salario * 0.12;
+    }
+    else {
+        inss = salario * 0.14;
+    }
+
+	return 0;
+}
 	
 	
 
